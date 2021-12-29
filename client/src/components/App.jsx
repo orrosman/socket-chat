39eeacd5997d6d6a81778786f11f4189ce19aab7
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-12-29 16:35:38 +0200

    working socket connection server-client

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { io } from 'socket.io-client';
 
 const App = () => {
+	const socketRef = useRef();
+
+	useEffect(() => {
+		socketRef.current = io('http://localhost:8080', {
+			transports: ['websocket'],
+		});
+		socketRef.current.on('connect', () => {
+			console.log(socketRef.current.id);
+		});
+	});
+
 	return <div className="App text-center">put some content here</div>;
 };
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
-const req = require('express/lib/request');
 const app = express();
+const cors = require('cors');
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
 const port = 8080;
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+	console.log('a user connected id: ' + socket.id);
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
+});
 
 app.get('/', (req, res) => {
 	res.send('server is working!👍');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`server running on port ${port}`);
 });
